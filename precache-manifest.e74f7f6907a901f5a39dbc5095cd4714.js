d43091cfec0efc7f3f6ba844a3af4620e84c8b20
Andrew <andrewpeliza@hotmail.com>
2018-12-11 00:44:12 +0000

    Updates

---
+++ precache-manifest.e74f7f6907a901f5a39dbc5095cd4714.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d7770b5b2ec6a6eb1c0",
+    "url": "/todo-list/static/css/main.565a92fe.chunk.css"
+  },
+  {
+    "revision": "3d7770b5b2ec6a6eb1c0",
+    "url": "/todo-list/static/js/main.3d7770b5.chunk.js"
+  },
+  {
+    "revision": "30af51ddb1ce01eec1f0",
+    "url": "/todo-list/static/js/1.30af51dd.chunk.js"
+  },
+  {
+    "revision": "6f47f260b12c7ad9f72e",
+    "url": "/todo-list/static/js/runtime~main.6f47f260.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-list/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "aec53b327a66c28bf2b1334cce70cc2d",
+    "url": "/todo-list/index.html"
+  }
+];
